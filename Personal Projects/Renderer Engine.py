80e334efe541f93a7c7e2c06897a3cf99a75e977
Pakistan5681 <paxton.hall@ucas-edu.net>
2025-10-30 18:18:22 +0000

    The Project Begins

---
+++ Personal Projects/Renderer Engine.py
@@ -0,0 +1,46 @@
+import pygame as py
+
+py.init()
+screen = py.display.set_mode((1280, 720))
+clock = py.time.Clock()
+running = True
+
+red = (255, 0, 0)
+
+class Vertex:
+    def __init__(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
+
+class Face:
+    def __init__(self, vertOne, vertTwo, vertThree, color):
+        self.vertOne = vertOne
+        self.vertTwo = vertTwo
+        self.vertThree = vertThree
+        self.color = color
+
+def drawFace(face, screen):
+    vert1 = [face.vertOne.x, face.vertOne.y]
+    vert2 = [face.vertTwo.x, face.vertTwo.y]
+    vert3 = [face.vertThree.x, face.vertThree.y]
+
+    py.draw.polygon(screen, face.color)
+
+vert1 = Vertex(0, 0, 0)
+vert2 = Vertex(2, 0, 0)
+vert3 = Vertex(1, 2, 0)
+
+face =  Face(vert1, vert2, vert3, red)
+
+print(face.vertTwo)
+
+while True:
+    screen.fill("purple")
+
+
+
+    py.display.flip()
+
+
+
