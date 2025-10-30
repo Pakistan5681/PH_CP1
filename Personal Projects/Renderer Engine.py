43d880998be1981cce3949dd8c6c7f582c21768e
Pakistan5681 <paxton.hall@ucas-edu.net>
2025-10-30 16:56:26 -0600

    Goofy Ah Rotation

--- Personal Projects/Renderer Engine.py
+++ Personal Projects/Renderer Engine.py
@@ -32,27 +32,52 @@ def drawFace(face, screen):
     py.draw.polygon(screen, face.color, [vert1, vert2, vert3])
 
 def rotateVertex(vertex, centerPoint, rotationType, angle):
+    angle = m.radians(angle)
     relativeVertex = [vertex.x - centerPoint.x, vertex.y - centerPoint.y, vertex.z - centerPoint.z]
-    newVertexLocal = Vertex(0, 0, 0)
+    newVertexLocal = [0, 0, 0]
 
     if rotationType == "x":
-        newVertexLocal.x = relativeVertex[0]
-        newVertexLocal.y = (relativeVertex[1] * m.cos(angle)) - (relativeVertex[2] * m.cos(angle))
-        newVertexLocal.z = (relativeVertex[1] * m.sin(angle)) + (relativeVertex[2] * m.sin(angle))
+        newVertexLocal[0] = relativeVertex[0]
+        newVertexLocal[1] = (relativeVertex[1] * m.cos(angle)) - (relativeVertex[2] * m.sin(angle))
+        newVertexLocal[2] = (relativeVertex[1] * m.sin(angle)) + (relativeVertex[2] * m.cos(angle))
+    elif rotationType == "y":
+        newVertexLocal[1] = relativeVertex[1]
+        newVertexLocal[0] = (relativeVertex[0] * m.cos(angle)) + (relativeVertex[2] * m.sin(angle))
+        newVertexLocal[2] = (-relativeVertex[0] * m.sin(angle)) + (relativeVertex[2] * m.cos(angle))
+    elif rotationType == "z":
+        newVertexLocal[2] = relativeVertex[1]
+        newVertexLocal[0] = (relativeVertex[0] * m.cos(angle)) - (relativeVertex[1] * m.sin(angle))
+        newVertexLocal[1] = (relativeVertex[0] * m.sin(angle)) + (relativeVertex[1] * m.cos(angle))
+    else:
+        print(f"{rotationType} is not a valid rotation")
+    
+    newVertexLocal[0] += centerPoint.x
+    newVertexLocal[1] += centerPoint.y
+    newVertexLocal[2] += centerPoint.z
 
-    return newVertexLocal
+    print(newVertexLocal)
+    return Vertex(newVertexLocal[0], newVertexLocal[1], newVertexLocal[2])
+
+def rotateFace(face, rotationType, centerPoint, angle):
+    face.vertOne = rotateVertex(face.vertOne, centerPoint, rotationType, angle)
+    face.vertTwo = rotateVertex(face.vertTwo, centerPoint, rotationType, angle)
+    face.vertThree = rotateVertex(face.vertThree, centerPoint, rotationType, angle)
+
+    return face
 
 vert1 = Vertex(0, 0, 0)
-vert2 = Vertex(2, 0, 0)
-vert3 = Vertex(1, 2, 0)
+vert2 = Vertex(200, 0, 0)
+vert3 = Vertex(100, 200, 0)
 
 face =  Face(vert1, vert2, vert3, red)
 
 while running:
     screen.fill("purple")
+    clock.tick(60)
 
     drawFace(face, screen)
-
+    face = rotateFace(face, "z", Vertex(100, 200, 0), 1)
+    face = rotateFace(face, "x", Vertex(100, 200, 0), 1)
     py.display.flip()
 
 
